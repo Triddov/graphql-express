28d982847de31d30efce67930bc88c819be44687
Triddov <akinin.stepan7@gmail.com>
2025-06-23 18:28:05 +0300

    feat: add schema and base graphql operations

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { createHandler } from "graphql-http/lib/use/express";
+import schema from './schema.js';
+
+
+dotenv.config({path: '.dev.env'});
+const APP_PORT = process.env.PORT || 80;
+const ANY_USERS_DATA = [{id: 1, username: "Step", email: "123@123.com", age: 20}];
+
+const app = express();
+
+// это резолвер - функция, возвращающая данные конктретной схемы
+const root = {
+    getAllUsers: () => { return ANY_USERS_DATA },
+    getUser: ({id}) => { return ANY_USERS_DATA.find(user => user.id === id) }
+}
+
+
+app.use(express.json());
+app.use(cors());
+
+app.all('/graphql', createHandler({
+    graphiql: true,
+    schema,
+    rootValue: root
+}))
+
+
+app.listen(APP_PORT, () => console.log(` === Listening on ${APP_PORT}=== `));
+
+
+
+
+
+
---
+++ schema.js
@@ -0,0 +1,41 @@
+import { buildSchema } from 'graphql';
+
+const schema = buildSchema(`
+    
+    type User {
+        id: ID,
+        username: String,
+        email: String,
+        age: Int,
+        posts: [Post]
+    }
+    type Post {
+        id: ID,
+        title: String,
+        content: String 
+    }
+    
+    input UserInput {
+        id: ID,
+        username: String!,
+        email: String!,
+        age: Int,
+        posts: [PostInput]
+    } 
+    input PostInput {
+        id: ID,
+        title: String!,
+        content: String! 
+    }
+    
+    type Query {
+        getAllUsers: [User],
+        getUser(id: ID): User
+    }
+`);
+
+// инпуты - это мутации для создания новых объектов
+// квери - это запросы
+// фрагменты - это переиспользуемые части квери
+
+export default schema;
